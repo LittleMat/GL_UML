76ccf7586c2aee56b34145a736ac3230080afce2
mathieu <matmont98@gmail.com>
2019-05-14 08:28:34 +0200

    creation of class capteur

---
+++ src/capteur.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+                           Capteur  -  description
+                             -------------------
+    beginning            : 14/05
+    copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
+    e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
+*************************************************************************/
+
+//------------- Realisation of <Capteur> (file Capteur.cpp) ----------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//--------------------------------------------------------- System include
+
+//------------------------------------------------------ Personnal include
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Public methodes
+
+//-------------------------------------------- Constructors - destructor
+
+//----- End of ~Capteur 
---
+++ src/capteur.h
@@ -0,0 +1,32 @@
+/*************************************************************************
+                           Capteur  -  description
+                             -------------------
+    beginning            : 14/05
+    copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
+    e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface of <Capteur> (file Capteur.h) ----------------
+
+//------------------------------------------------------------------------
+// Goal of the <Capteur> class
+//------------------------------------------------------------------------
+
+
+class Capteur
+{
+//----------------------------------------------------------------- PUBLIC
+
+public :
+
+//----------------------------------------------------- Public methods
+
+//-------------------------------------------- Constructor - destructor
+
+protected :
+
+//----------------------------------------------------- Protected attributes
+
+};
+
+#endif // PARSER_H
