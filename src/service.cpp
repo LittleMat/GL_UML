e97b8fa958b41df04f0a3fd3297cb2aa0fce469d
Linda Ye <lye@insa-lyon.fr>
2019-05-14 09:29:42 +0200

    debut service

--- src/service.cpp
+++ src/service.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
-                           Service  -  description
-                             -------------------
-    beginning            : 14/05
-    copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
-    e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
+Service  -  description
+-------------------
+beginning            : 14/05
+copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
+e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
 *************************************************************************/
 
 //------------- Realisation of <Service> (file Service.cpp) ----------------
@@ -11,15 +11,37 @@
 //---------------------------------------------------------------- INCLUDE
 
 //--------------------------------------------------------- System include
+#include <iostream>
+using namespace std;
 
 //------------------------------------------------------ Personnal include
+#include "Service.h"
 
 //------------------------------------------------------------- Constantes
 
 //----------------------------------------------------------------- PUBLIC
 
-//----------------------------------------------------- Public methodes
+//----------------------------------------------------- Public methods
 
-//-------------------------------------------- Constructors - destructor
+ //-------------------------------------------- Constructor - destructor
+Service::Service()
+// Algorithm : 
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Service>" << endl;
+#endif
 
-//----- End of ~Service 
+}//----- End of Service 
+
+Service::~Service()
+// Algorithm : 
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <Service>" << endl;
+#endif
+}//----- End of ~Service 
+
+
+ //------------------------------------------------------------------ PRIVE
+
+ //----------------------------------------------------- Protected methods
--- src/service.h
+++ src/service.h
@@ -1,14 +1,23 @@
 /*************************************************************************
-                           Service  -  description
-                             -------------------
-    beginning            : 14/05
-    copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
-    e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
+Service  -  description
+-------------------
+beginning            : 14/05
+copyright            : (C) 2019 by CERBA Guilhem, YE Zihang, YE Linda, MONTGOMERY Mathieu, ZHANG Tianyu
+e-mail               : guilhem.cerba@insa-lyon.fr, zihang.ye@insa-lyon.fr, linda.ye@insa-lyon.fr , mathieu.montgomery@insa-lyon.fr, tianyu.zhang@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface of <Service> (file Service.h) ----------------
-#ifndef SERVICE_H
+#if ! defined ( SERVICE_H )
 #define SERVICE_H
+
+//--------------------------------------------------- Interfaces used
+#include <iostream>
+using namespace std;
+
+#include "FileReader.h"
+#include <list>
+#include "Capteur.h"
+#include <tuple>
 
 //------------------------------------------------------------------------
 // Goal of the <Service> class
@@ -17,18 +26,21 @@
 
 class Service
 {
-//----------------------------------------------------------------- PUBLIC
+	//----------------------------------------------------------------- PUBLIC
 
-public :
+public:
 
-//----------------------------------------------------- Public methods
+	//----------------------------------------------------- Public methods
+	bool surveillerComportementCapteur(string capteurID);
+	list<Capteur> surveillerComportementCapteurs(list<string> capteursID );
+	list<tuple(Capteur, Capteur)> obtenirCapteursSimilaires(debut : Date, nbMesures : int);
+	tuple(int, list<float>, int) calculerQualite(tempsInf : Date, tempsSup : Date)
 
-//-------------------------------------------- Constructor - destructor
+	//-------------------------------------------- Constructor - destructor
 
-protected :
-
-//----------------------------------------------------- Protected attributes
-
+protected:
+	//----------------------------------------------------- Protected attributes
+	FileReader fileReader;
 };
 
 #endif // SERVICE_H
