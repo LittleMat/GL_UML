fa5464a1ec41118dcd65992ed46d8c4cb496ff25
Linda1998 <ylinda1998@gmail.com>
2019-06-14 00:28:42 +0200

    modif capteurs similaires à tester avec les valeurs de Zihang

--- src/Service.cpp
+++ src/Service.cpp
@@ -28,6 +28,7 @@ list<string> *Service ::surveillerComportementCapteurs(list<string> &capteursID,
 	if (capteursID.empty())
 		throw "Illegal Argument Exception: empty list";
 
+	fileReader->lireAttributs();
 	Territoire territoire(Point(0.0, 0.0), M_PI * RAYON_TERRE);
 	string empty;
 	using namespace placeholders;
@@ -44,7 +45,7 @@ list<string> *Service ::surveillerComportementCapteurs(list<string> &capteursID,
 	};
 	unordered_map<string, Capteur *> capteurs = fileReader->lireCapteurs(predicateCapteurs);
 
-	//liste d'id de capteurs defectueux
+	//liste d'id de capteurs defectueux vide
 	list<string> *liste_id_capteursDefectueux = new list<string>;
 
 	fileReader->debutMesure();
@@ -53,7 +54,7 @@ list<string> *Service ::surveillerComportementCapteurs(list<string> &capteursID,
 	while (!finLecture)
 	{
 		// On selectionne les mesures qui satisfont les criteres de selection temporels
-		const Mesure *m = fileReader->prochaineMesure(predicateMesureWithCapteur);
+		const Mesure * m = fileReader->prochaineMesure(predicateMesureWithCapteur);
 
 		//Si m == nullptr, alors il n'y a plus rien a lire
 		if (m == nullptr)
@@ -82,7 +83,6 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 // hypothese 1 : on considere que deux capteurs ont pris leurs mesures au meme moment si la difference entre leurs mesures est de +- 1 minute
 // hypothese 2 : un capteur a un instant t doit a des valeurs non null pour les 4 particules de l'air
 // hypothese 3 : en considerant l'hypothese 1 valide, on suppose que tous les capteurs prennent leurs mesures en meme temps
-// (vrai dans le debut du fichier .csv de mesures que j'ai lu)
 {
 
 	fileReader->debutMesure();
@@ -92,21 +92,19 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 	Territoire earth(Point(0.0, 0.0), M_PI * RAYON_TERRE);
 	string empty;
 	function<bool(Capteur&)> predicateCapteur = bind(Service::filtrageCapteur,  _1, cref(earth), empty);
-
 	unordered_map<std::string, Capteur *> map_capteurs = fileReader->lireCapteurs(predicateCapteur);
-
-	cout << "taille capteur = " << map_capteurs.size() << endl;
 
 	//On recupere tous les attributs
 	unordered_map<std::string, Attribut *> map_attributs = fileReader->lireAttributs();
 
+	//Structure de données pour classer les mesures 
+	// unordered_map<idCapteur, unordered_map <idAttribut,vector<Mesure>>> 
 	unordered_map<string, unordered_map<string, vector<float>>> capteurs_mesures;
-	//unordered_map<sensorID, unordered_map<AttributId, vector<value> > > capteurs_mesures;
+
 	function<bool(Mesure&)> predicateMesure = bind(Service::filtrageMesure, _1, tm(), date);
 
 	// On classe les donnees pour faciliter le traitement
-
-	// Init
+	// Initialisation avec des -1 
 	for (unordered_map<std::string, Capteur *>::iterator it = map_capteurs.begin(); it != map_capteurs.end(); it++)
 	{
 
@@ -124,7 +122,7 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 		capteurs_mesures.insert(make_pair(it->first, map_temp));
 	}
 
-	//Remplissage
+	//Remplissage avec les mesures lues
 	for (int i = 0; i < nbMesures; i++)
 	{
 		Mesure *m = fileReader->prochaineMesure(predicateMesure);
@@ -133,7 +131,7 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 			break;
 
 		// Etape de selection du capteur
-		// Si dans la map des capteurs selectionnes par les fonctions de filtrage spaciaux
+		// Si dans la map des capteurs selectionnes par les fonctions de filtrage spaciales
 		// le capteur de la mesure est present, c'est qu'il faut anaylser ce capteur
 
 		unordered_map<std::string, Capteur *>::iterator trouveCapteur = map_capteurs.find(m->getSensorID());
@@ -160,6 +158,7 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 	}
 
 	// Remplissage des zones vides
+	/*
 	for (unordered_map<string, unordered_map<string, vector<float>>>::iterator it = capteurs_mesures.begin(); it != capteurs_mesures.end(); it++)
 	{
 		for (unordered_map<string, vector<float>>::iterator it_2 = it->second.begin(); it_2 != it->second.end(); it_2++)
@@ -173,29 +172,6 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 			}
 		}
 	}
-
-	/*
-	// Visualiser la structure 
-	std :: cout << "Visualisation de la structure apres lecture" << endl;
-	std::cout << endl;
-	std::cout << "----------------------------------------------" << endl;
-	for (unordered_map< string, unordered_map< string, vector<float> > > ::iterator it = capteurs_mesures.begin(); it != capteurs_mesures.end(); it++)
-	{
-		std::cout << "Capteur = " << it->first << endl;
-		for (unordered_map< string, vector<float> > ::iterator it_2 = it->second.begin(); it_2 != it->second.end(); it_2++)
-		{
-			std::cout << "Attribut = " << it_2->first << endl;
-			for (vector<float> ::iterator it_3 = it_2->second.begin(); it_3 != it_2->second.end(); it_3++)
-			{
-				std::cout << *it_3 << " ; ";
-			}
-			std::cout << endl;
-		}
-		std::cout << endl;
-	}
-	std::cout << "----------------------------------------------" << endl;
-	std::cout << endl;
-	std::cout << endl;
 	*/
 
 	// Traitement
@@ -221,6 +197,14 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 
 				for (int i = 0; i < nbMesures; i++)
 				{
+					if (it_c1_O3->second[i] == -1 || it_c2_O3->second[i] == -1
+						|| it_c1_NO2->second[i] == -1 || it_c2_NO2->second[i] == -1
+						|| it_c1_SO2->second[i] == -1 || it_c2_SO2->second[i] == -1
+						|| it_c1_PM10->second[i] == -1 || it_c2_PM10->second[i] == -1)
+					{
+						similaire = false;
+						break;
+					}
 					similaire = similaire && (plusOuMoins(it_c1_O3->second[i], it_c2_O3->second[i], 15) &&
 											  plusOuMoins(it_c1_NO2->second[i], it_c2_NO2->second[i], 20) &&
 											  plusOuMoins(it_c1_SO2->second[i], it_c2_SO2->second[i], 15) &&
@@ -236,17 +220,15 @@ list<pair<string, string>> *Service ::obtenirCapteursSimilaires(struct tm & date
 	}
 	return capteurs_similaires;
 
-} //----- End of obtenirCapteursSimilaires
+}//----- End of obtenirCapteursSimilaires
 
 bool Service::plusOuMoins(float v1, float v2, float ecart)
 {
 	return (abs(v1 - v2) <= ecart);
-}
+}//----- End of plusOuMoins
 
 tuple<int, list<pair<string, float>>, float> Service::calculerQualite(string &capteurId, function<bool(Mesure &)> predicateMesure, function<float(Capteur &)> fiabilite)
 {
-
-	/*VESION OPTIMISEE OU ON NE STOCKE PAS LES MESURES*/
 
 	fileReader->debutMesure();
 
@@ -379,175 +361,6 @@ tuple<int, list<pair<string, float>>, float> Service::calculerQualite(string &ca
 
 	return resultat_final;
 
-	/* VERSION OU ON STOCKE TOUT */
-	/*
-	fileReader->debutMesure();
-	unordered_map <string, Attribut *> attributs = fileReader->lireAttributs();
-	unordered_map <string, Capteur *> capteurs = fileReader->lireCapteurs(parametres, filtrageCapteur);
-	list <pair<float, float>> mesures_O3;
-	list <pair<float, float>> mesures_NO2;
-	list <pair<float, float>> mesures_SO2;
-	list <pair<float, float>> mesures_PM10;
-
-	bool finLecture = false;
-	while (finLecture == false)
-	{
-		// On recupere et lit une mesure
-		const Mesure * m = fileReader->prochaineMesure(parametres, filtrageMesure);
-		
-		if (m == nullptr)
-		{
-			finLecture = true;
-			break;
-		}
-
-		// On calule l'indice de fiabilite
-		float fiabilite = 1.0; //il est de 1 pour l'aire totale ou un capteur en particulier
-
-		//On ne considere pas un capteur en particulier
-		if (parametres.capteurId.compare("") == 0)
-		{
-			// Un point  
-			if (parametres.territoire->getRayon() == 0 )
-			{
-
-				fiabilite = 1 - parametres.territoire->getCentre()->distance(capteurs.at(m->getSensorID())->getPosition())/10;
-			}
-			// Un territoire cible
-			else if (parametres.territoire->getRayon() > 0)
-				
-			{
-				fiabilite = 1 - (parametres.territoire->getCentre()->distance(capteurs.at(m->getSensorID())->getPosition()) - parametres.territoire->getRayon()) / (0.1*parametres.territoire->getRayon());
-			}
-
-		}
-		if (fiabilite < 0)
-			fiabilite = 0;
-
-		// On insere la mesure dans la liste appropriee
-		if (m->getAttributID().compare("O3") == 0)
-		{
-			mesures_O3.push_back(make_pair(m->getValue(), fiabilite));
-		}
-		else if (m->getAttributID().compare("SO2") == 0)
-		{
-			mesures_SO2.push_back(make_pair(m->getValue(), fiabilite));
-
-		}
-		else if (m->getAttributID().compare("NO2") == 0)
-		{
-			mesures_NO2.push_back(make_pair(m->getValue(), fiabilite));
-
-		}
-		else if (m->getAttributID().compare("PM10") == 0)
-		{
-			mesures_PM10.push_back(make_pair(m->getValue(), fiabilite));
-		}
-
-		delete m;
-	}
-
-	//Calcul des concentrations moyennes + fiabilite	
-	list<pair<string, float>> concentrations;
-	list<pair<string, float>> fiabilites;
-	float somme_concentrationMoyenne = 0.0;
-	float concentrationMoyenne = -1.0;
-	float somme_fiabiliteMoyenne = 0.0; 
-	float fiabiliteMoyenne = -1.0;
-
-	if (!mesures_O3.empty())
-	{
-		for (list<pair<float, float>> ::iterator it = mesures_O3.begin(); it != mesures_O3.end(); it++)
-		{
-			somme_concentrationMoyenne += it->first;
-			somme_fiabiliteMoyenne += it->second;
-		}
-		concentrationMoyenne = somme_concentrationMoyenne / mesures_O3.size();
-		fiabiliteMoyenne = somme_fiabiliteMoyenne / mesures_O3.size();
-
-	}
-	concentrations.push_back(make_pair("O3", concentrationMoyenne));  // Si == -1 => pas de mesures : autres solutions ??
-	fiabilites.push_back(make_pair("O3", fiabiliteMoyenne));
-
-	somme_concentrationMoyenne = 0.0;
-	concentrationMoyenne = -1.0;
-	if (!mesures_SO2.empty())
-	{
-		for (list<pair<float, float>> ::iterator it = mesures_SO2.begin(); it != mesures_SO2.end(); it++)
-		{
-			somme_concentrationMoyenne += it->first;
-			somme_fiabiliteMoyenne += it->second;
-
-		}
-		concentrationMoyenne = somme_concentrationMoyenne / mesures_SO2.size();
-		fiabiliteMoyenne = somme_fiabiliteMoyenne / mesures_SO2.size();
-	}
-	concentrations.push_back(make_pair("SO2", concentrationMoyenne));  // Si == -1 => pas de mesures : autres solutions ??
-	fiabilites.push_back(make_pair("SO2", fiabiliteMoyenne));
-
-	somme_concentrationMoyenne = 0.0;
-	concentrationMoyenne = -1.0;
-	if (!mesures_NO2.empty())
-	{
-		for (list<pair<float, float>> ::iterator it = mesures_NO2.begin(); it != mesures_NO2.end(); it++)
-		{
-			somme_concentrationMoyenne += it->first;
-			somme_fiabiliteMoyenne += it->second;
-
-		}
-		concentrationMoyenne = somme_concentrationMoyenne / mesures_NO2.size();
-		fiabiliteMoyenne = somme_fiabiliteMoyenne / mesures_NO2.size();
-	}
-	concentrations.push_back(make_pair("NO2", concentrationMoyenne));  // Si == -1 => pas de mesures : autres solutions ??
-	fiabilites.push_back(make_pair("NO2", fiabiliteMoyenne));
-
-	somme_concentrationMoyenne = 0.0;
-	concentrationMoyenne = -1.0;
-	if (!mesures_PM10.empty())
-	{
-		for (list<pair<float, float>> ::iterator it = mesures_PM10.begin(); it != mesures_PM10.end(); it++)
-		{
-			somme_concentrationMoyenne += it->first;
-			somme_fiabiliteMoyenne += it->second;
-
-		}
-		concentrationMoyenne = somme_concentrationMoyenne / mesures_PM10.size();
-		fiabiliteMoyenne = somme_fiabiliteMoyenne / mesures_PM10.size();
-	}
-	concentrations.push_back(make_pair("PM10", concentrationMoyenne));  // Si == -1 => pas de mesures : autres solutions ??
-	fiabilites.push_back(make_pair("PM10", fiabiliteMoyenne));
-
-
-	float indiceMax = -1.0;
-	int indiceATMO;
-	string composant;
-	for (list<pair<string, float>> ::iterator it = concentrations.begin(); it != concentrations.end(); it++)
-	{
-		indiceATMO = calculIndiceATMO(it->first, it->second);
-		if (indiceATMO > indiceMax)
-		{
-			indiceMax = indiceATMO;
-		}
-	}
-
-	indiceATMO = indiceMax;
-
-	
-
-	float fiabiliteMin = 1;
-	for (list<pair<string, float>> ::iterator it = fiabilites.begin(); it != fiabilites.end(); it++)
-	{
-		if (fiabiliteMin >= it->second)
-		{
-			fiabiliteMin = it->second;
-		}
-	}
-
-	tuple<int, list<pair<string, float>>, float>  resultat_final = make_tuple(indiceATMO, concentrations, fiabiliteMin * 100);
-	*/
-
-	return resultat_final;
-
 } //----- End of calculerQualite
 
 //-------------------------------------------- Constructor - destructor
@@ -570,7 +383,7 @@ Service::~Service()
 	delete fileReader;
 } //----- End of ~Service
 
-int Service ::calculIndiceATMO(string substance, float valeur)
+int Service :: calculIndiceATMO (string substance, float valeur)
 {
 	int indiceATMO = -1;
 	if (substance.compare("O3") == 0)
@@ -746,7 +559,7 @@ int Service ::calculIndiceATMO(string substance, float valeur)
 		}
 	}
 	return indiceATMO;
-}
+}//----- End of calculIndiceATMO
 
 //------------------------------------------------------------------ PRIVE
 
